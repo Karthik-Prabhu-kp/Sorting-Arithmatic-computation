e2039d0e11baad1cad52b8169767ff56003c4c71
Karthik-Prabhu-kp <kpwat66@gmail.com>
2020-09-03 09:52:43 +0530

    adding value to dict

---
+++ dict_store.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+read -p "enter first value " a
+read -p "enter second value " b
+read -p "enter third value " c
+
+operation1=$(( $a+$b*$c ))
+operation2=$(( $a*$b+$c ))
+operation3=$(( $c+$a/$b ))
+operation4=$(( $a%$b+$c ))
+
+declare -A Comp
+comp[op1]=$operation1
+comp[op2]=$operation2
+comp[op3]=$operation3
+comp[op4]=$operation4
+
+
